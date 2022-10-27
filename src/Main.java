495a701cd5f5cc6517d6ec0e2634bae6449185f2
Salar86 <99397531+Salar86@users.noreply.github.com>
2022-10-27 12:02:20 +0200

    changes in electric car

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Car c1 = new GasolineCar("AJ22548", "Honda", "Civic", 5, 25);
         Car c2 = new GasolineCar("DH36598", "Toyota", "Yaris", 5, 20);
         Car c3 = new ElectricCar("BB46987", "Nissan", "LEAF", 3,24, 175);
-        Car c4 = new ElectricCar("FO08456", "Toyota", "RAV4", 5,42, 166);
+        Car c4 = new ElectricCar("FO08456", "Toyota", "RAV4", 5,28, 153);
         Car c5 = new DieselCar("SS34654", "Ford","Ka", 4, 20, true);
         Car c6 = new DieselCar("LT52147", "Mercedes", "CLA200D", 5, 21, false);
         fleet.addCar(c1);
